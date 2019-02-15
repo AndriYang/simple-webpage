c8261c564759126f5fb75f083fc935ae0f8b3401
AndriYang <44841881+AndriYang@users.noreply.github.com>
2019-02-15 15:59:27 +0800

    complete simple webpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Snorlex from '../snorlex.png'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-  state = {
-    posts: [ ]
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts') //asynchronised
-      .then(res => {
-        console.log(res)
-        this.setState ({
-          posts: res.data.slice(0,10) //to get the first ten request
-        })
-      }) //.then() run when the get completed
-  }
+  // state = {
+  //   posts: [ ]
+  // }
+  // componentDidMount(){
+  //   axios.get('https://jsonplaceholder.typicode.com/posts') //asynchronised
+  //     .then(res => {
+  //       console.log(res)
+  //       this.setState ({
+  //         posts: res.data.slice(0,10) //to get the first ten request
+  //       })
+  //     }) //.then() run when the get completed
+  // }
   render() {
-    const { posts } = this.state;
+    console.log(this.props)
+    //const { posts } = this.state;
+    const { posts } = this.props;
     //cycle through the post when there is items in the API
     const postList = posts.length ? (
       posts.map(post => {
@@ -46,4 +49,10 @@ class Home extends Component {
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
